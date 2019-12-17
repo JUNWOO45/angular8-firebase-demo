a06ef98ddda1c8f03bb60c57469ae5b208e2c9bb
junwoo45 <sinyoorang@gmail.com>
2019-12-17 11:44:20 +0900

    Observable 사용 및 unsubscribe 테스트

--- src/app/customers/customer-list/customer-list.component.ts
+++ src/app/customers/customer-list/customer-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, timer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.css']
 })
-export class CustomerListComponent implements OnInit {
+export class CustomerListComponent implements OnInit, OnDestroy {
+  private subscription: Subscription = new Subscription();
+
+  // timer$: Observable<number> = timer(0, 1000);
+
+  everySecond$: Observable<number> = timer(0, 1000);
+  everyMinute$: Observable<number> = timer(0, 60000);
 
   constructor() { }
 
   ngOnInit() {
+    // this.everySecond$.subscribe(second => {
+    //   console.log(second);
+    // })
+    this.subscription.add(this.everySecond$.subscribe(second => console.log(second)));
+    this.subscription.add(this.everyMinute$.subscribe(minute => console.log(minute)));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
